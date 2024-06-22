521cac63b665e790b75b98e4c0b502ff3c15c04e
PedrohenriqueNovaisNeves <161246901+PedrohenriqueNovaisNeves@users.noreply.github.com>
2024-06-21 22:07:53 -0300

    ClasseMain

---
+++ CalculadoraJava.java
@@ -0,0 +1,16 @@
+package calculadorajava;
+import telas.*;
+
+import telas.telaInicial;
+
+
+public class CalculadoraJava {
+
+
+    public static void main(String[] args) {
+        
+        telaInicial tela = new telaInicial();
+        tela.setVisible(true);
+    }
+    
+}
